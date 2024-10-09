cea8838ebb892dd8dbb1ff5ac8bc0beb3245109f
AnantaBG <anantabanikofficial@gmail.com>
2024-10-10 04:13:43 +0600

    Added Sorting by buttons

--- js/features.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('btn-Donation').addEventListener('click', function(){
-    console.log('Donation button')
-    showSectionByID('donation')
-})
-document.getElementById('btn-History').addEventListener('click', function(){
-    console.log('History button')
-    showSectionByID('history')
-})
